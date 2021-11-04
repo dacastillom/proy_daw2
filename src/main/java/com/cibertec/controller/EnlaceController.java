2e7c5a344a8851904f3846756837686f15c34012
kamalyzapana <kamaly@b89.io>
2021-11-04 01:18:10 -0500

    feature: se elimna rutas en ingresos

--- src/main/java/com/cibertec/controller/EnlaceController.java
+++ src/main/java/com/cibertec/controller/EnlaceController.java
@@ -26,22 +26,6 @@ public class EnlaceController {
 	@RequestMapping("/verRegistroProducto")
 	public String verRegistraProducto() {	return "intranetRegistroProducto";	}
 
-	@RequestMapping("/verRegistroReclamo")
-	public String verRegistraReclamo() {	return "intranetRegistroReclamo";	}
-	
-	@RequestMapping("/verRegistroSede")
-	public String verRegistroSede() {	return "intranetRegistroSede";	}
-	
-	@RequestMapping("/verRegistroUsuario")
-	public String verRegistroUsuario() {	return "intranetRegistroUsuario";	}
-	
-	@RequestMapping("/verRegistroTipoReclamo")
-	public String verRegistroTipoReclamo() {	return "intranetRegistroTipoReclamo";	}
-	
-	@RequestMapping("/verRegistroPais")
-	public String verRegistroPais() {	return "intranetRegistroPais";	}
-	
-	
 	//cruds
 	@RequestMapping("/verCrudCliente")
 	public String verCrudCliente() {	return "intranetCrudCliente";	}
